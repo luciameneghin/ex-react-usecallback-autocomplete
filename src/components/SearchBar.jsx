d428f167a56e3f660f07cbaa37a4b858be1cc017
luciameneghin <lucia.meneghin00@gmail.com>
2025-04-23 11:17:18 +0200

    added some style

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,6 +10,8 @@ function debounce(callback, delay) {
 
 
 import { useEffect, useState, useCallback } from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
 
 const SearchBar = () => {
   const [search, setSearch] = useState('');
@@ -36,24 +38,28 @@ const SearchBar = () => {
   }, [search])
 
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={(e => setSearch(e.target.value))}
-        placeholder='Cerca...'
-        className='form-control w-50 mx-auto bg-light'
-      />
+    <>
+      <div className="d-flex align-items-center">
+        <input
+          type="text"
+          value={search}
+          onChange={(e => setSearch(e.target.value))}
+          placeholder='Cerca...'
+          className='form-control w-50 mx-auto bg-light border-5 border-warning border-opacity-50 rounded shadow-sm fw-bold'
+        />
+        <FaSearch className="search-icon" />
+      </div>
       {search && filteredItems.length > 0 && (
         <ul className="list-group">
           {filteredItems.map((item, index) => (
-            <li key={index} onClick={() => handleSelect(item.name)} className="list-group-item w-50 mx-auto">
-              <p>{item.name}</p>
+            <li key={index} onClick={() => handleSelect(item.name)} className="list-group-item w-50 mx-auto d-flex align-items-center">
+              <IoSearch />
+              <p className="px-3 my-2">{item.name}</p>
             </li>
           ))}
         </ul>
       )}
-    </div>
+    </>
   )
 }
 
